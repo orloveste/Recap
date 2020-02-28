72330e0454a491f32c966c1ca610d1f7862d3bb0
orloveste <orlovevm@yahoo.com>
2020-02-28 10:47:49 +0200

    reDo else

---
+++ src/main/java/Hibernate/HibernateUtilPerson.java
@@ -0,0 +1,17 @@
+package Hibernate;
+
+//import org.hibernate.cfg.Configuration;
+//import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+public class HibernateUtilPerson {
+//    Configuration configuration = new Configuration();
+//
+//    Properties settings = new Properties();
+//    settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+//    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/");
+//    settings.put(Environment.USER, "root");
+//    settings.put(Environment.PASS, "root");
+
+}
