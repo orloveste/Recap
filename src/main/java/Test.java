f9b5fbead6203993e83bc71fd332aa72f00732b5
orloveste <orlovevm@yahoo.com>
2020-03-21 11:56:09 +0200

    Test - 'sout' diference, 'psvm' and 'main' shotcut are same

---
+++ src/main/java/Test.java
@@ -0,0 +1,17 @@
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("basic");
+        System.out.printf("ceva\n");
+        System.out.println("Test.main");
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("28293 = " + 28293);
+        System.out.println("true = " + true);
+        System.out.println("false = " + false);
+        System.out.println("args = " + args);
+
+    }
+}
