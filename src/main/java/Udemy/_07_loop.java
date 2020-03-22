f296b73bcec47367678e0e98f77f579e9f88e2d8
orloveste <orlovevm@yahoo.com>
2020-03-22 17:02:26 +0200

    Udemy - Loop - for meth.

--- src/main/java/Udemy/_07_loop.java
+++ src/main/java/Udemy/_07_loop.java
@@ -2,9 +2,12 @@ package Udemy;
 
 public class _07_loop {
     public static void main(String[] args) {
+        System.out.println("while");
         whileLoop();
-        System.out.println("doWhile");
+        System.out.println("\ndoWhile");
         doWhileLoop();
+        System.out.println("\nfor Loop");
+        forLoop();
     }
 
     static void whileLoop(){
@@ -12,13 +15,19 @@ public class _07_loop {
 
         while (counter <10){
             counter = counter+1;
-            System.out.println(counter);
+            System.out.print(counter+" ");
+
         }
     }
     static void doWhileLoop(){
         int counter = 11;
         do{
-            System.out.println(counter++);
+            System.out.print(counter++ +" ");
         }while (counter <= 20);
+    }
+    static void forLoop(){
+        for (int counter = 21; counter <= 30; counter = counter+1){
+            System.out.print(counter+" ");
+        }
     }
 }
