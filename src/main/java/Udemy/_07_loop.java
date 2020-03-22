4358cff9ee631beb442443ab1e83601611af3f79
orloveste <orlovevm@yahoo.com>
2020-03-22 16:50:18 +0200

    Test - Loop - while meth.

---
+++ src/main/java/Udemy/_07_loop.java
@@ -0,0 +1,16 @@
+package Udemy;
+
+public class _07_loop {
+    public static void main(String[] args) {
+        whileLoop();
+    }
+
+    static void whileLoop(){
+        int counter = 0;
+
+        while (counter <10){
+            counter = counter+1;
+            System.out.println(counter);
+        }
+    }
+}
