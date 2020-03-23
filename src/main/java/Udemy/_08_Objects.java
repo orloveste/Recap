feacccb41f030b9e2de44a7fcd1f709a5e32b747
orloveste <orlovevm@yahoo.com>
2020-03-23 11:29:48 +0200

    Udemy - window JFrame test meth - explore more when will be needed

--- src/main/java/Udemy/_08_Objects.java
+++ src/main/java/Udemy/_08_Objects.java
@@ -1,6 +1,7 @@
 package Udemy;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class _08_Objects {
     public static void main(String[] args) {
@@ -10,8 +11,32 @@ public class _08_Objects {
         window.setSize(800, 600);
         window.setVisible(true);
 
+        window.getGraphics();
+//        Image jetbrains;
+//        window.setIconImage(jetbrains://idea/navigate/reference?project=RecapHibernateSDA&path=~\Pictures\7E.PNG);
+//        window.setDefaultCloseOperation("OrlovStyle", 1);
+        window.getAccessibleContext();
+        window.getJMenuBar();
+        window.getGlassPane();
+        window.getDefaultCloseOperation();
+        window.getTransferHandler();
+        window.getContentPane();
+        window.getRootPane();
+
         JLabel label =  new JLabel();
+        label.getDisplayedMnemonic();
         label.setText("Covid-19 is on going");
+        label.getHorizontalAlignment();
+        label.getIconTextGap();
+        label.getLabelFor();
+        label.getIcon();
+        label.getAccessibleContext();
+        label.getUI();
+        label.getUIClassID();
+        label.getVerticalAlignment();
+//        label.setHorizontalTextPosition(SwingConstants.TOP);
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        label.setHorizontalTextPosition(SwingConstants.RIGHT);
 
         window.add(label);
     }
