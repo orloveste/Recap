ee764da6b1f638679ca60a5cb3a01253c14c3345
orloveste <orlovevm@yahoo.com>
2020-03-23 12:40:22 +0200

    Udemy - Classes and objects - get set

--- src/main/java/Udemy/Person.java
+++ src/main/java/Udemy/Person.java
@@ -3,4 +3,29 @@ package Udemy;
 public class Person {
     String name;
     int age;
+    String sayHello = " said hello to ";
+    public Person(String name){
+        this.name = name;
+        System.out.println("Create person");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void sayHelloTo(Person generic) {
+        System.out.println(getName() + sayHello + generic.getName());
+    }
 }
--- src/main/java/Udemy/_09_ClassesAndObjects.java
+++ src/main/java/Udemy/_09_ClassesAndObjects.java
@@ -1,19 +1,17 @@
 package Udemy;
 
-import javax.swing.*;
 
 public class _09_ClassesAndObjects {
     public static void main(String[] args) {
-        JLabel label = new JLabel();
+        Person chirita = new Person("Chiritoaia");
+        chirita.setAge(20);
+        Person newPope = new Person("Vasile");
+        newPope.setAge(44);
 
-        Person chirita = new Person();
-        chirita.name = "Chiritoaia";
-        chirita.age = 20;
-        Person popa = new Person();
-        popa.name = "Vasile";
-        popa.age = 25;
+        chirita.sayHelloTo(newPope);
+        newPope.sayHelloTo(chirita);
 
-        System.out.println(chirita.name +" is "+ chirita.age+ " years old");
-        System.out.println(popa.name +" is "+ popa.age+ " years old");
+        System.out.println(chirita.getName() +" is "+ chirita.getAge()+ " years old");
+        System.out.println(newPope.getName()+" is "+ newPope.getAge()+ " years old");
     }
 }
