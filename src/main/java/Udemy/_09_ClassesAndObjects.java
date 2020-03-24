c0899fe08fb4d34c4e220fcde0672ce679cdf966
orloveste <orlovevm@yahoo.com>
2020-03-24 16:32:04 +0200

    Udemy - Classes and objects - with readear

--- src/main/java/Udemy/_09_ClassesAndObjects.java
+++ src/main/java/Udemy/_09_ClassesAndObjects.java
@@ -1,11 +1,13 @@
 package Udemy;
 
 
+import java.util.Scanner;
+
 public class _09_ClassesAndObjects {
     public static void main(String[] args) {
-        Person chirita = new Person("Chiritoaia");
+        _09_Person chirita = new _09_Person("Chiritoaia");
         chirita.setAge(20);
-        Person newPope = new Person("Vasile");
+        _09_Person newPope = new _09_Person("Vasile");
         newPope.setAge(44);
 
         chirita.sayHelloTo(newPope);
@@ -13,5 +15,15 @@ public class _09_ClassesAndObjects {
 
         System.out.println(chirita.getName() +" is "+ chirita.getAge()+ " years old");
         System.out.println(newPope.getName()+" is "+ newPope.getAge()+ " years old");
+
+        Scanner reader = new Scanner(System.in);
+        //String newEntry = reader.nextLine();
+        System.out.print("Enter new namePerson: ");
+        _09_Person newEntry = new _09_Person(reader.nextLine());
+        System.out.print("enter new age: ");
+        newEntry.setAge(reader.nextInt());
+        System.out.println(newEntry.getName()+ " is "+ newEntry.getAge()+ " years old");
+        newPope.sayHelloTo(newEntry);
+
     }
 }
--- src/main/java/Udemy/_09_Person.java
+++ src/main/java/Udemy/_09_Person.java
@@ -1,12 +1,12 @@
 package Udemy;
 
-public class Person {
+public class _09_Person {
     String name;
     int age;
     String sayHello = " said hello to ";
-    public Person(String name){
+
+    public _09_Person(String name){
         this.name = name;
-        System.out.println("Create person");
     }
 
     public String getName() {
@@ -25,7 +25,7 @@ public class Person {
         this.age = age;
     }
 
-    public void sayHelloTo(Person generic) {
+    public void sayHelloTo(_09_Person generic) {
         System.out.println(getName() + sayHello + generic.getName());
     }
 }
