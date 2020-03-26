622ae9bf8128193187f81cd30002553e40f3b6f4
orloveste <orlovevm@yahoo.com>
2020-03-26 20:29:32 +0200

    Udemy - Access Modifiers - clasify

---
+++ src/main/java/Udemy/AccessModifiers.java
@@ -0,0 +1,23 @@
+package Udemy;
+// 11
+public class AccessModifiers {
+    private int x;
+    public int y;
+    public static void main(String[] args) {
+        
+    }
+    public void doSomethingPublic(){
+
+    }
+    protected void doSomethingProtected(){
+
+    }
+    void  doSomethingVoid(){
+
+    }
+    private void doSomethingPrivate(){
+
+    }
+
+
+}
