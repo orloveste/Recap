39b07395375f4c1d3321fa5c0ac6af7cba2916ce
orloveste <orlovevm@yahoo.com>
2020-03-26 21:07:41 +0200

    Udemy - Constants - can modify data definetly and use that-

--- src/main/java/Udemy/tutorials/basic/_14_Constants.java
+++ src/main/java/Udemy/tutorials/basic/_14_Constants.java
@@ -1,7 +1,16 @@
 package Udemy.tutorials.basic;
 
 public class _14_Constants {
+    //Create own constants
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final _09_Person Pi = new _09_Person("");
+    public static final int NUM_ENEMIES =5;
+
     public static void main(String[] args) {
+
+
+        System.out.println(WIDTH);
         System.out.println(Math.PI);
     }
 }
