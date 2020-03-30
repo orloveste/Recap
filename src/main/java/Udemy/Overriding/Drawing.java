49a6d225d791ad234a5ac563357754bed84f133e
orloveste <orlovevm@yahoo.com>
2020-03-30 18:57:18 +0300

    Udemy - 16. Overriding Methods: drawing on a frame - test window

---
+++ src/main/java/Udemy/Overriding/Drawing.java
@@ -0,0 +1,13 @@
+package Udemy.Overriding;
+
+import javax.swing.*;
+
+public class Drawing extends JFrame {
+    public static void main(String[] args) {
+        new Drawing();
+    }
+    public Drawing() {
+       setSize(600, 600);
+       setVisible(true);
+    }
+}
