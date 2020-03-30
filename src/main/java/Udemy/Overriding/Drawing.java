afc8e1f7d9976d10bb353650f171c953c3e649b6
orloveste <orlovevm@yahoo.com>
2020-03-30 19:06:18 +0300

    Udemy - 16. Overriding Methods: drawing on a frame - draw in w

--- src/main/java/Udemy/Overriding/Drawing.java
+++ src/main/java/Udemy/Overriding/Drawing.java
@@ -1,6 +1,7 @@
 package Udemy.Overriding;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Drawing extends JFrame {
     public static void main(String[] args) {
@@ -9,5 +10,14 @@ public class Drawing extends JFrame {
     public Drawing() {
        setSize(600, 600);
        setVisible(true);
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        //super.paint(g);
+        g.drawArc(150,50,34,53,535,355);
+        g.draw3DRect(50,50,100,25, false);
+        g.drawRect(150,150, 100, 25);// x/y coordonates
     }
 }
