d1aad7f40e7e12a1192052787e2f96af0ae34b68
orloveste <orlovevm@yahoo.com>
2020-03-30 19:17:06 +0300

    Udemy - 16. Overriding Methods: drawing on a frame - draw in collor W

--- src/main/java/Udemy/Overriding/Drawing.java
+++ src/main/java/Udemy/Overriding/Drawing.java
@@ -18,6 +18,11 @@ public class Drawing extends JFrame {
         //super.paint(g);
         g.drawArc(150,50,34,53,535,355);
         g.draw3DRect(50,50,100,25, false);
+        g.setColor(new Color(0,0,255));
         g.drawRect(150,150, 100, 25);// x/y coordonates
+        g.setColor(Color.CYAN);
+        g.fill3DRect(250, 150, 100,25, true);
+        g.setColor(Color.MAGENTA);
+        g.drawString("Covid-19", 250, 250);
     }
 }
