2139a52674d1fa3c9b8a99bafa4513c467ad4472
orloveste <orlovevm@yahoo.com>
2020-03-30 18:39:27 +0300

    RedHat - Quarkus acces Spring -noGo- Terminal nope

--- src/main/java/RedHat/Lambda_and_serverless/GreetingsLambda.java
+++ src/main/java/RedHat/Lambda_and_serverless/GreetingsLambda.java
@@ -1,18 +1,21 @@
 package RedHat.Lambda_and_serverless;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
-public class GreetingsLambda implements RequestHandler {
+@RestController
+public class GreetingsLambda{
     @Inject
     GreetingService service;
 
-    @Override
-    public String handleRequest(Object o, Context context) {
+    @RequestMapping(path = "/greeting/{name}", method = RequestMethod.GET, produces = "text/plain")
+
+    public String handleRequest(@PathVariable("name") String name) {
         return service.greeting(name);
     }
-
-
 }
