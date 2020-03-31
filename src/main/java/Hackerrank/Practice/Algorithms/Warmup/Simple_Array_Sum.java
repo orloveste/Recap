dd6804485276a90261914d5d84e57a7ed8a3d884
orloveste <orlovevm@yahoo.com>
2020-03-31 17:24:39 +0300

    Hackerrank - done sum - adapted for IntelliJ

--- src/main/java/Hackerrank/Practice/Algorithms/Warmup/Simple_Array_Sum.java
+++ src/main/java/Hackerrank/Practice/Algorithms/Warmup/Simple_Array_Sum.java
@@ -1,43 +1,24 @@
 package Hackerrank.Practice.Algorithms.Warmup;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Simple_Array_Sum {
-    /*
-     * Complete the simpleArraySum function below.
-     */
     static int simpleArraySum(int[] ar) {
-        /*
-         * Write your code here.
-         */
-
+        int lenAr = ar.length;
+        int sum = 0;
+        for (int i=0; i<lenAr; i++){
+            sum += ar[i];
+        }return sum;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int arCount = Integer.parseInt(scanner.nextLine().trim());
-
-        int[] ar = new int[arCount];
-
-        String[] arItems = scanner.nextLine().split(" ");
-
-        for (int arItr = 0; arItr < arCount; arItr++) {
-            int arItem = Integer.parseInt(arItems[arItr].trim());
-            ar[arItr] = arItem;
-        }
+    public static void main(String[] args) {
+        int[] ar = new int[]{1,2,3,4,10,11};
+        System.out.println("out array is: "+ Arrays.toString(ar));
 
         int result = simpleArraySum(ar);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
+        System.out.println("sum is: "+ result);
     }
 }
 //https://www.hackerrank.com/challenges/simple-array-sum/problem
