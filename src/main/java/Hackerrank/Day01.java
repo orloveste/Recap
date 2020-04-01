049c2a86c96ee944ed8b3a18365d72d4c28ea2e4
orloveste <orlovevm@yahoo.com>
2020-04-01 10:49:11 +0300

    Hackerrank - read and sum int, double, concatenate strings

---
+++ src/main/java/Hackerrank/Day01.java
@@ -0,0 +1,36 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+public class Day01 {
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
+// Exp output 1
+//16
+//8.0
+//HackerRank is the best place to learn and practice coding!
+
+//Exp output 2
+//7
+//6.8
+//HackerRank is my favorite platform!
