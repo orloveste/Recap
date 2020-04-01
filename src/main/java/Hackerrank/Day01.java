6483f0da5a8b47ff3f08afac73eb33d86e370ad6
orloveste <orlovevm@yahoo.com>
2020-04-01 11:21:51 +0300

    Hackerrank - noGo- read and sum int snd double - not read string -

--- src/main/java/Hackerrank/Day01.java
+++ src/main/java/Hackerrank/Day01.java
@@ -11,18 +11,31 @@ public class Day01 {
         Scanner scan = new Scanner(System.in);
 
         /* Declare second integer, double, and String variables. */
+        int i2;
+        double d2;
+        String s2;
 
         /* Read and save an integer, double, and String to your variables.*/
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        System.out.println("int");
+        i2 = scan.nextInt();
+        System.out.println("double");
+        d2 = scan.nextDouble();
+        System.out.println("string");
+        s2 = scan.nextLine();
+        //"is the best place to learn and practice coding!"
+
 
         /* Print the sum of both integer variables on a new line. */
-
+        int sum =i+i2;
+        System.out.println(sum);
         /* Print the sum of the double variables on a new line. */
-
+        double sumD =d+d2;
+        System.out.println(sumD);
         /* Concatenate and print the String variables on a new line;
         	the 's' variable above should be printed first. */
-
-        scan.close();
+        System.out.println(s+s2);
+        //scan.close();
     }
 }
 // Exp output 1
