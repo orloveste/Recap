3906a1dc242a366d85021c4d93e98e333f0cc6a7
orloveste <orlovevm@yahoo.com>
2020-04-01 14:41:37 +0300

    Udemy - read all methods with 'for'

--- src/main/java/Udemy/Game/GameObject.java
+++ src/main/java/Udemy/Game/GameObject.java
@@ -5,9 +5,18 @@ public abstract class GameObject {
 
     public static void main(String[] args) {
         GameObject player = new Player();
-        player.draw();
+//        player.draw();
         GameObject menu = new Menu();
-        menu.draw();
+//        menu.draw();
+
+        GameObject[] gameObjects = new GameObject[2];
+        gameObjects[1] = player;
+        gameObjects[0] = menu;
+
+        for (GameObject obj : gameObjects){
+            obj.draw();
+        }
+
         //GameObject mwObject = new GameObject();
     }
 }
