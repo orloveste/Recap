f895f46a7b64bc335ddbb3405eaccb10c3e95bb5
orloveste <orlovevm@yahoo.com>
2020-04-01 11:39:23 +0300

    Hackerrank - done - read and sum int snd double  read string - toDo else

--- src/main/java/Hackerrank/Day01.java
+++ src/main/java/Hackerrank/Day01.java
@@ -10,40 +10,33 @@ public class Day01 {
 
         Scanner scan = new Scanner(System.in);
 
-        /* Declare second integer, double, and String variables. */
         int i2;
         double d2;
         String s2;
 
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
         System.out.println("int");
         i2 = scan.nextInt();
+
         System.out.println("double");
         d2 = scan.nextDouble();
+
         System.out.println("string");
-        s2 = scan.nextLine();
-        //"is the best place to learn and practice coding!"
+        scan.nextLine();// todo is not reading string 's2' if is not here
+        s2 = scan.nextLine();//
 
+        System.out.println(i+i2);
 
-        /* Print the sum of both integer variables on a new line. */
-        int sum =i+i2;
-        System.out.println(sum);
-        /* Print the sum of the double variables on a new line. */
-        double sumD =d+d2;
-        System.out.println(sumD);
-        /* Concatenate and print the String variables on a new line;
-        	the 's' variable above should be printed first. */
+        System.out.println(d+d2);
+
         System.out.println(s+s2);
-        //scan.close();
     }
 }
 // Exp output 1
-//16
-//8.0
-//HackerRank is the best place to learn and practice coding!
+//16 // 12
+//8.0 // 4
+//HackerRank // is the best place to learn and practice coding!
 
 //Exp output 2
-//7
-//6.8
-//HackerRank is my favorite platform!
+//7 // 3
+//6.8 // 2.8
+//HackerRank // is my favorite platform!
