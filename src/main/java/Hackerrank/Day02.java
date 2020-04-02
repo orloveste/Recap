63b26660c840923b71ef93e2a090056cfee1ae68
orloveste <orlovevm@yahoo.com>
2020-04-02 12:29:07 +0300

    Hackerrank - Operators - cost adding double

--- src/main/java/Hackerrank/Day02.java
+++ src/main/java/Hackerrank/Day02.java
@@ -6,23 +6,21 @@ public class Day02 {
     // Complete the solve function below.
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
 
+        double totalCost = meal_cost + (meal_cost * (tip_percent))/100 + (meal_cost*(tax_percent))/100;
+        System.out.println(totalCost);
+
 
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double meal_cost = scanner.nextDouble();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int tip_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int tax_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        double meal_cost = 12.00;
+        int tip_percent = 20;
+        int tax_percent = 8;
 
         solve(meal_cost, tip_percent, tax_percent);
 
-        scanner.close();
+
     }
 }
