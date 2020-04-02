661d31c6b75a5576f94c12ea8901abb4d004f979
orloveste <orlovevm@yahoo.com>
2020-04-02 12:56:53 +0300

    Hackerrank - Operators -done- round add price + tax + tip

--- src/main/java/Hackerrank/Day02.java
+++ src/main/java/Hackerrank/Day02.java
@@ -1,26 +1,18 @@
 package Hackerrank;
 
-import java.util.Scanner;
-
 public class Day02 {
-    // Complete the solve function below.
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double totalCost = meal_cost + (meal_cost * tip_percent)/100
+                + (meal_cost*tax_percent)/100;
 
-        double totalCost = meal_cost + (meal_cost * (tip_percent))/100 + (meal_cost*(tax_percent))/100;
         System.out.println(totalCost);
-
-
+        System.out.println("round: " + Math.round(totalCost));
     }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        double meal_cost = 12.00;
-        int tip_percent = 20;
-        int tax_percent = 8;
+        double meal_cost = 15.50;
+        int tip_percent = 15;
+        int tax_percent = 10;
 
         solve(meal_cost, tip_percent, tax_percent);
-
-
     }
 }
