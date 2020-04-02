c760355d57699a9570dd2bf423ac2f029aae6558
orloveste <orlovevm@yahoo.com>
2020-04-02 19:14:34 +0300

    Hackerrank - Warm-up Challenge- Sock_Merchant.java- done- compare Integers in list and take out nr of pairs

--- src/main/java/Hackerrank/Practice/Algorithms/Warmup/Sock_Merchant.java
+++ src/main/java/Hackerrank/Practice/Algorithms/Warmup/Sock_Merchant.java
@@ -1,28 +1,28 @@
 package Hackerrank.Practice.Algorithms.Warmup;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Sock_Merchant {
-//     Complete the sockMerchant function below.
     static int sockMerchant(int n, int[] ar) {
-        int counter = 0;
-        int i = 0;
-        while (i < n) {
-            int element = ar[i];
-            if (i == element ){
-                counter++;
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int intermediar = 0; intermediar < n; intermediar++) {
+            int elementOfArray = ar[intermediar];
+            if (set.contains(elementOfArray)) {
+                set.remove(elementOfArray);
+                count++;
             } else {
-
+                set.add(elementOfArray);
             }
         }
-        return counter;
+        return count;
     }
 
     public static void main(String[] args) {
+        int[] ar = new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20,30,20,10,20};
 
-        int n = 9;
-        int[] ar = new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20};
-
+        int n = ar.length;
 
         System.out.println(Arrays.toString(ar));
         int result = sockMerchant(n, ar);
