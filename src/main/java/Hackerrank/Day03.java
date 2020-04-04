b81a149e5f05a3c094c209eb38792ce3238508c5
orloveste <orlovevm@yahoo.com>
2020-04-04 12:58:45 +0300

    hackerrank - Day3 - random nr even odd 3prints
    hackerrank.com/challenges/30-conditional-statements/tutorial

--- src/main/java/Hackerrank/Day03.java
+++ src/main/java/Hackerrank/Day03.java
@@ -1,23 +1,30 @@
 package Hackerrank;
 
+import java.util.Random;
+
 public class Day03 {
+    public String randomNumber(){
+        Random random = new Random();
+        int n = Math.abs(random.nextInt());
+        System.out.print(n+" ");
+        n %= 2;
+        if (n == 0){
+            return "Weird - even";
+        } else {
+            return "Not Weird - odd";
+        }
+    }
 
-
-    static void weird(){
+    static String weird(){
         System.out.println("Weird");
+        return weird();
     }
 
     public static void main(String[] args) {
+       Day03 game = new Day03();
+        System.out.println(game.randomNumber());
+        System.out.println(game.randomNumber());
+        System.out.println(game.randomNumber());
 
-        int N = 0;
-//        if ((N / 2 != 0)&&(2<=N<=5 )){
-//
-//
-//            weird();
-//
-//        } else {
-//            System.out.print("Not ");
-//            weird();
-//        }
     }
 }
