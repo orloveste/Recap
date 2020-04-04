aa46ebb748265b382cc431d7fa67f576db437f52
orloveste <orlovevm@yahoo.com>
2020-04-04 15:42:23 +0300

    hackerrank - Day3 -notDoneYet-  printing not work properly to do condition even 2-5  and <20 - not weird to do 6-20  for even nr and all odd nr - Weird
    hackerrank.com/challenges/30-conditional-statements/tutorial

--- src/main/java/Hackerrank/Day03.java
+++ src/main/java/Hackerrank/Day03.java
@@ -9,12 +9,22 @@ public class Day03 {
         int n = Math.abs(random.nextInt(101));
         System.out.print(MessageFormat.format("{0} ", n));
         n %= 2;
-        if ( (2 <= n && n <= 5)||((n < 20) && (n == 0)) ) {
-            return "Not Weird - even";
+        if( n == 0 ){
+            if (2 <= n && n <= 5)  {
+                System.out.println("Not Weird 2-if");
+            }
+            else if (6 <= n && n <= 20) {
+                System.out.println("Weird else-if1");
+            }
+            else if( 20 < n) {
+                System.out.println("Not Weird else-if2");
+            }
+            return null;
         }
-        else {//toDo even nr 6-20 will be here
+        else {
             return "Weird - odd";
         }
+
     }
 //    public int condition1(){
 //
