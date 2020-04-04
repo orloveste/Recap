2695a058a435c39464c314c98d02d18023ee3922
orloveste <orlovevm@yahoo.com>
2020-04-04 18:18:59 +0300

    hackerrank - Day4 - tutorial -test ok- GuessTheNumber
    https://www.hackerrank.com/challenges/30-class-vs-instance/tutorial
    https://www.hackerrank.com/challenges/30-class-vs-instance/problem?

--- src/main/java/Hackerrank/Class_vs/_Instance/GuessTheNumber.java
+++ src/main/java/Hackerrank/Class_vs/_Instance/GuessTheNumber.java
@@ -7,34 +7,38 @@ public class GuessTheNumber {
     int theNumber;
     int max;
     Scanner reader = new Scanner(System.in);
-    public GuessTheNumber(){
+    protected GuessTheNumber(){
         Random random = new Random();
-        theNumber = Math.abs(random.nextInt())%(max+1);
-        //random.nextInt();
+        max = 100;
+        theNumber = Math.abs(random.nextInt()) % (max + 1);
     }
     public void play(){
-        int move = reader.nextInt();
-        if ( move> theNumber ){
-            System.out.println("to big");
-        } else if ( move< theNumber ){
-            System.out.println("Is too small");
-        }else {
-            System.out.println("Got it");
+        while (true) {
+            int move = reader.nextInt();
+            if ( move > theNumber ) {
+                System.out.print("to big: _");
+            } else if ( move < theNumber ) {
+                System.out.print("Is too small: _");
+            } else {
+                System.out.println("Got it !!!");
+                break;
+            }
         }
     }
 
     public static void howBigIsMyNumber(int x){
         if ( x >= 0 && x <= 10 ){
-            System.out.println("Number is pretty small");
+            System.out.print("Number is pretty small: _");
         } else if ( x >= 11 && x <= 100  ){
-            System.out.println("Number is pretty big");
+            System.out.print("Number is pretty big: _");
         }else {
-            System.out.println("Number is out of space");
+            System.out.println("Number is out of space! _");
         }
     }
 
     public static void main(String[] args) {
-
+        GuessTheNumber guessGame = new GuessTheNumber();
+        System.out.print("Wecome, guess number 0-100: ?");
+        guessGame.play();
     }
-
 }
