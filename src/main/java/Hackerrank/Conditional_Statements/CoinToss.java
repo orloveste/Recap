f900707915a32c3da69575dda78898abcb7959ef
orloveste <orlovevm@yahoo.com>
2020-04-04 11:18:37 +0300

    hackerrank.com/challenges/30-conditional-statements/tutorial - CoinToss - random nr even odd statement if+else

--- src/main/java/Hackerrank/Conditional_Statements/CoinToss.java
+++ src/main/java/Hackerrank/Conditional_Statements/CoinToss.java
@@ -4,10 +4,14 @@ import java.util.Random;
 
 public class CoinToss {
 
-    public int tossCoin(){
+    public String tossCoin(){
         Random random = new Random();
         int toss = Math.abs(random.nextInt()) % 2;
-        return toss;
+        if (toss == 0){
+            return "Heads";
+        } else {
+            return "tails";
+        }
     }
     public static void main(String[] args) {
         CoinToss game = new CoinToss();
