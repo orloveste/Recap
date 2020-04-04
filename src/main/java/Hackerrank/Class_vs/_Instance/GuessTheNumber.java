740ebc9b3ed1b17e5b55c9ac21467d222e39c4e1
orloveste <orlovevm@yahoo.com>
2020-04-04 17:58:26 +0300

    hackerrank - Day4 - tutorial GuessTheNumber - test ok- if,if-else,else state print
    https://www.hackerrank.com/challenges/30-class-vs-instance/tutorial
    https://www.hackerrank.com/challenges/30-class-vs-instance/problem?

--- src/main/java/Hackerrank/Class_vs/_Instance/GuessTheNumber.java
+++ src/main/java/Hackerrank/Class_vs/_Instance/GuessTheNumber.java
@@ -1,4 +1,25 @@
 package Hackerrank.Class_vs._Instance;
 
 public class GuessTheNumber {
+    public static void howBigIsMyNumber(int x){
+        if ( x >= 0 && x <= 10 ){
+            System.out.println("Number is pretty small");
+        } else if ( x >= 11 && x <= 100  ){
+            System.out.println("Number is pretty big");
+        }else {
+            System.out.println("Number is out of space");
+        }
+    }
+
+    public static void main(String[] args) {
+        howBigIsMyNumber(0);
+        howBigIsMyNumber(10);
+        howBigIsMyNumber(9);
+        howBigIsMyNumber(75);
+        howBigIsMyNumber(-1);
+        howBigIsMyNumber(105);
+        howBigIsMyNumber(18);
+        howBigIsMyNumber(76);
+    }
+
 }
