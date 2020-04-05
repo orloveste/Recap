af6041b32474382d62384e043a9f560771546b73
orloveste <orlovevm@yahoo.com>
2020-04-05 12:04:06 +0300

    hackerrank - Day4 -accepted version- determines the age category - Sample Input 4 -1 10 16 18
    https://www.hackerrank.com/challenges/30-class-vs-instance/tutorial
    https://www.hackerrank.com/challenges/30-class-vs-instance/problem?

--- src/main/java/Hackerrank/Day04.java
+++ src/main/java/Hackerrank/Day04.java
@@ -6,23 +6,19 @@ import java.util.Scanner;
 public class Day04 {
     private int age;
 
-    public Day04(int initialAge, int age) {
-        this.age = age;
-        // Add some more code to run some checks on initialAge
+    public Day04(int initialAge) {
+        this.age = initialAge;
         if ( initialAge < 0) System.out.println("Age is not valid, setting age to 0.");
     }
 
     public void amIOld() {
-        // Write code determining if this person's age is old and print the correct statement:
         if ( age < 13 ) System.out.println("You are young.");
         else if ( age < 18 ) System.out.println("You are a teenager.");
         else System.out.println("You are old dude.");
     }
 
     public void yearPasses() {
-        // Increment this person's age.
-        age++;
-
+        this.age++;
     }
 
     public static void main(String[] args) {
@@ -30,7 +26,7 @@ public class Day04 {
         int T = sc.nextInt();
         for (int i = 0; i < T; i++) {
             int age = sc.nextInt();
-            Day04 p = new Day04(age, age);
+            Day04 p = new Day04(age);
             p.amIOld();
             for (int j = 0; j < 3; j++) {
                 p.yearPasses();
