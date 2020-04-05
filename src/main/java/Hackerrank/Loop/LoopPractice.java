4cc08d3c83e73d4325e249c826811a2706750d90
orloveste <orlovevm@yahoo.com>
2020-04-05 15:47:50 +0300

    hackerrank - tutorial- For -Loops -increment
    https://www.hackerrank.com/challenges/30-loops/tutorial

--- src/main/java/Hackerrank/Loop/LoopPractice.java
+++ src/main/java/Hackerrank/Loop/LoopPractice.java
@@ -16,9 +16,18 @@ public class LoopPractice {
             x++;
         } while (x < 10);
     }
+    public static void practiceForLoop(){
+        for (int x =0; x < 20; x++){
+            System.out.println("For Value of x is "+x);
+            if ( x == 10 ) {
+                break;
+            }
+        }
+        System.out.println("The end!");
+    }
 
     public static void main(String[] args) {
-        precticeDoWhileLoop();
+        practiceForLoop();
 
     }
 }
