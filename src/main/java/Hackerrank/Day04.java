f01aae78af64153dc6c9fe4d79e5e06637ec7961
orloveste <orlovevm@yahoo.com>
2020-04-05 11:48:40 +0300

    hackerrank - Day4 -Test ok- determines the age category - Sample Input 4 -1 10 16 18
    https://www.hackerrank.com/challenges/30-class-vs-instance/tutorial
    https://www.hackerrank.com/challenges/30-class-vs-instance/problem?

--- src/main/java/Hackerrank/Day04.java
+++ src/main/java/Hackerrank/Day04.java
@@ -6,28 +6,23 @@ import java.util.Scanner;
 public class Day04 {
     private int age;
 
-    public Day04(int initialAge) {
+    public Day04(int initialAge, int age) {
+        this.age = age;
         // Add some more code to run some checks on initialAge
-
-        if ( initialAge < 0){
-            System.out.println("Age is not valid, setting age to 0.");
-        }
+        if ( initialAge < 0) System.out.println("Age is not valid, setting age to 0.");
     }
 
     public void amIOld() {
         // Write code determining if this person's age is old and print the correct statement:
-        if ( age < 13 ){
-            System.out.println("You are young.");
-        } else if ( age >= 13 && age < 18 ){
-            System.out.println("You are a teenager.");
-        } else {
-            System.out.println(" You are old dude.");
-        }
+        if ( age < 13 ) System.out.println("You are young.");
+        else if ( age < 18 ) System.out.println("You are a teenager.");
+        else System.out.println("You are old dude.");
     }
 
     public void yearPasses() {
         // Increment this person's age.
         age++;
+
     }
 
     public static void main(String[] args) {
@@ -35,7 +30,7 @@ public class Day04 {
         int T = sc.nextInt();
         for (int i = 0; i < T; i++) {
             int age = sc.nextInt();
-            Day04 p = new Day04(age);
+            Day04 p = new Day04(age, age);
             p.amIOld();
             for (int j = 0; j < 3; j++) {
                 p.yearPasses();
