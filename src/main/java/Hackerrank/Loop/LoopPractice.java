ec5c69b25f0fce4d9e7e6105d4c06fd65cf70029
orloveste <orlovevm@yahoo.com>
2020-04-05 15:39:19 +0300

    hackerrank - tutorial- while -Loops
    https://www.hackerrank.com/challenges/30-loops/tutorial

--- src/main/java/Hackerrank/Loop/LoopPractice.java
+++ src/main/java/Hackerrank/Loop/LoopPractice.java
@@ -1,4 +1,19 @@
 package Hackerrank.Loop;
 
 public class LoopPractice {
+
+    public static void practiceWhileLoop(){
+        int x=0;
+        while ( x < 5){
+            System.out.println("Value of x is "+x);
+            x++;
+        }
+    }
+
+    public static void main(String[] args) {
+        practiceWhileLoop();
+    }
 }
+// while
+// do-while
+// for
