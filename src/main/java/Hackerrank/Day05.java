cdf23b38984fdac3351770b3ad1b66da9e363d04
orloveste <orlovevm@yahoo.com>
2020-04-05 13:05:09 +0300

    hackerrank - Day5 -dive in- Loops
    https://www.hackerrank.com/challenges/30-loops

---
+++ src/main/java/Hackerrank/Day05.java
@@ -0,0 +1,18 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+//Day 5: Loops
+public class Day05 {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
