cc03816639c83d96682dece2540303d6bd876095
orloveste <orlovevm@yahoo.com>
2020-04-05 15:44:18 +0300

    hackerrank - tutorial- doWhile -Loops
    https://www.hackerrank.com/challenges/30-loops/tutorial

--- src/main/java/Hackerrank/Loop/LoopPractice.java
+++ src/main/java/Hackerrank/Loop/LoopPractice.java
@@ -3,15 +3,23 @@ package Hackerrank.Loop;
 public class LoopPractice {
 
     public static void practiceWhileLoop(){
-        int x=0;
+        int x = 0;
         while ( x < 5){
             System.out.println("Value of x is "+x);
             x++;
         }
     }
+    public static void precticeDoWhileLoop(){
+        int x = 0;
+        do {
+            System.out.println("doWhile Value of x is "+x);
+            x++;
+        } while (x < 10);
+    }
 
     public static void main(String[] args) {
-        practiceWhileLoop();
+        precticeDoWhileLoop();
+
     }
 }
 // while
