0e5f0f9e62c9687d9f0fa3c0fea39068ebb8a518
orloveste <orlovevm@yahoo.com>
2020-04-05 14:39:31 +0300

    hackerrank - Day5 -test ok- add any number with 1-10 Loops
    https://www.hackerrank.com/challenges/30-loops

--- src/main/java/Hackerrank/Day05.java
+++ src/main/java/Hackerrank/Day05.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 //Day 5: Loops
 public class Day05 {
+    int result;
+    int i;
 
-
+    private void adding(int n) {
+        for (this.i = 1; i < 11; i++) {
+           result= n*i;
+            System.out.println(n+" x "+i+" = "+result);
+        }
+    }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Your number: ");
         int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        scanner.close();
+        Day05 newAdd = new Day05();
+        newAdd.adding(n);
+
     }
+
+
 }
