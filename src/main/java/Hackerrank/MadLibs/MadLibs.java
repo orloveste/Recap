80878661023911ee63ae65ad32a6588b8bd1d958
orloveste <orlovevm@yahoo.com>
2020-04-06 18:07:22 +0300

    hackerrank - Day6 tutorial -on it- play word game?
    https://www.hackerrank.com/challenges/30-review-loop/problem

---
+++ src/main/java/Hackerrank/MadLibs/MadLibs.java
@@ -0,0 +1,23 @@
+package Hackerrank.MadLibs;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MadLibs {
+
+    Scanner scanner = new Scanner(System.in);
+    String story;
+    String name;
+    String adjective1;
+    String adjective2;
+    String noun1;
+    String noun2;
+    String noun3;
+    String adverb;
+    String randomNums;
+    Random random = new Random();
+
+    public static void main(String[] args) {
+
+    }
+}
