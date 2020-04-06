6de47cdcb77149bcdea6a7f05536303245014a5f
orloveste <orlovevm@yahoo.com>
2020-04-06 18:00:57 +0300

    hackerrank - Day6 -on it- string loop sort print
    https://www.hackerrank.com/challenges/30-review-loop/problem

---
+++ src/main/java/Hackerrank/Day06.java
@@ -0,0 +1,7 @@
+package Hackerrank;
+
+public class Day06 {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
