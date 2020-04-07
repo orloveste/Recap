7a98a920f934f3fa9a40063175a99d814ec30e1b
orloveste <orlovevm@yahoo.com>
2020-04-07 22:14:27 +0300

    hackerrank - Day6 -test ok- string loop sort print
                                                                https://www.hackerrank.com/challenges/30-review-loop/problem

--- src/main/java/Hackerrank/Day06.java
+++ src/main/java/Hackerrank/Day06.java
@@ -1,7 +1,27 @@
 package Hackerrank;
 
+import java.util.Scanner;
+
 public class Day06 {
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner reader = new Scanner(System.in);
+        System.out.println("How many times you whant check & separate \neven and odd char in provided word?:");
+        int numberForTest = reader.nextInt();
+        StringBuilder firstString = new StringBuilder();
+        StringBuilder secondString = new StringBuilder();
+        for (int i = 0; i < numberForTest; i++){
+            System.out.println("Please enter your word for transformation:");
+            String input = reader.next();
+            char[] sortChar = input.toCharArray();
+            for (int j = 0; j < sortChar.length; j++){
+                if ( j % 2 ==0 ){
+                    firstString.append(sortChar[j]);
+                } else {
+                    secondString.append(sortChar[j]);
+                }
+            }
+            System.out.println(firstString +" "+secondString);
+            firstString = new StringBuilder(secondString = new StringBuilder(""));
+        }
     }
 }
