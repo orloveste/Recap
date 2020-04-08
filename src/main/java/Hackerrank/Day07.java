df77076902a956a35166d1af82d3c3b5b6aedd64
orloveste <orlovevm@yahoo.com>
2020-04-08 16:03:26 +0300

    hackerrank - Day7 -on it- Arrays
                                                                https://www.hackerrank.com/challenges/30-arrays/problem

---
+++ src/main/java/Hackerrank/Day07.java
@@ -0,0 +1,25 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+//Day 7: Arrays
+public class Day07 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        scanner.close();
+    }
+}
