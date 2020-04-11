c4325f778a0b4ba985134b35d871aceeefb0e13f
orloveste <orlovevm@yahoo.com>
2020-04-11 15:31:29 +0300

    Hackerrank.com - Day 8: Dictionaries and Maps - on it

---
+++ src/main/java/Hackerrank/Day08.java
@@ -0,0 +1,20 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+public class Day08 {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+        }
+        in.close();
+    }
+}
