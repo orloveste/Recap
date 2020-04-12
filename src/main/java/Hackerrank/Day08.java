48780c9cacfcd7d0c4fbe68555404a9726355dc5
orloveste <orlovevm@yahoo.com>
2020-04-12 18:28:35 +0300

    Hackerrank.com - Day 8: Dictionaries and Maps -not ok- make map in for
    https://www.hackerrank.com/challenges/30-dictionaries-and-maps/tutorial

--- src/main/java/Hackerrank/Day08.java
+++ src/main/java/Hackerrank/Day08.java
@@ -1,16 +1,25 @@
 package Hackerrank;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Day08 {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter number of choices: ");
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
+            System.out.print("enter your friend name: ");
             String name = in.next();
+            System.out.println("Enter your friend number: ");
             int phone = in.nextInt();
             // Write code here
+            Map<String, Integer> phoneBook = new HashMap<>();
+            phoneBook.put(name, phone);
+            System.out.println("PhoneBook = "+ phoneBook.toString());
         }
+
         while(in.hasNext()){
             String s = in.next();
             // Write code here
