62e241762fd8d95e6d6c764e37afc2ef97e3fbe1
orloveste <orlovevm@yahoo.com>
2020-04-12 19:08:54 +0300

    Hackerrank.com - Day 8: Dictionaries and Maps -partial test ok-  enter data + check same number of entries
    https://www.hackerrank.com/challenges/30-dictionaries-and-maps/tutorial

--- src/main/java/Hackerrank/Day08.java
+++ src/main/java/Hackerrank/Day08.java
@@ -8,6 +8,7 @@ public class Day08 {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         Map<String, Integer> phoneBook = new HashMap<>();
+
         System.out.print("Number of new entries: ");
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
@@ -17,11 +18,25 @@ public class Day08 {
             int phone = in.nextInt();
             phoneBook.put(name, phone);
         }
-        System.out.println("PhoneBook = "+ phoneBook.toString());
-        while(in.hasNext()){
-            String s = in.next();
-            // Write code here
+        System.out.println("PhoneBook entry: "+ phoneBook.toString());
+
+        System.out.print("type 'exit' or Check name: ");
+        while(true){
+            if ( !in.hasNext() ) break;
+            for (int i = n; i >= 0; i--){
+                String s = in.next();
+                if (s.equals("exit") ){
+                    break;
+                }
+                else if ( phoneBook.containsKey(s) ) {
+                    System.out.println(s+"="+phoneBook.get(s));
+                }
+                else {
+                    System.out.println("Not found");
+                }
+            } break;
+
         }
-        in.close();
+
     }
 }
