96b3cde9ca58b0d9ad1c7c705aa99642688604ba
orloveste <orlovevm@yahoo.com>
2020-04-12 15:57:39 +0300

    Hackerrank.com - tutorials -Dictionaries and Maps - print map by key
    https://www.hackerrank.com/challenges/30-dictionaries-and-maps/tutorial

--- src/main/java/Hackerrank/DictionariesAndHashMaps/DictionaryPractice.java
+++ src/main/java/Hackerrank/DictionariesAndHashMaps/DictionaryPractice.java
@@ -1,7 +1,23 @@
 package Hackerrank.DictionariesAndHashMaps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DictionaryPractice {
     public static void main(String[] args) {
+//        English to Spanish Dictionary...
+        Map<String,String> englSpanDictionary = new HashMap<String,String>();
+        englSpanDictionary.put("Monday", "Lunes");
+        englSpanDictionary.put("Tuesday", "Martes");
+        englSpanDictionary.put("Wednesday", "Miércoles");
+        englSpanDictionary.put("Thursday", "Jueves");
+        englSpanDictionary.put("Friday", "Viernes");
+
+        System.out.println(englSpanDictionary.get("Monday"));
+        System.out.println(englSpanDictionary.get("Tuesday"));
+        System.out.println(englSpanDictionary.get("Wednesday"));
+        System.out.println(englSpanDictionary.get("Thursday"));
+        System.out.println(englSpanDictionary.get("Friday"));
 
     }
 }
