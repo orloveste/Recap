5531498068caa809c4e18543c441b1cae9b27661
orloveste <orlovevm@yahoo.com>
2020-04-13 15:50:35 +0300

    test scanner in

--- src/main/java/JetBrains/PrintData.java
+++ src/main/java/JetBrains/PrintData.java
@@ -1,9 +1,14 @@
 package JetBrains;
 
+import java.util.Scanner;
+
 public class PrintData {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
         }
+
+        Scanner scanner = new Scanner(System.in);
+        String thid = scanner.next();
     }
 }
