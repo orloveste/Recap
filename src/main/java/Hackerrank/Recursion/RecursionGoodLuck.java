d13cfbff5f552a47affc5a7876c67e9bd7ce377c
orloveste <orlovevm@yahoo.com>
2020-04-13 17:47:29 +0300

    HackerRank - Day 9: Recursion 3 - theory

---
+++ src/main/java/Hackerrank/Recursion/RecursionGoodLuck.java
@@ -0,0 +1,21 @@
+package Hackerrank.Recursion;
+
+public class RecursionGoodLuck {
+
+//    f(f(f(a))) ----- a= 20;
+//    f(a) = 5 + a
+
+//    f(20) =  5 + 20 =25
+//    f(f(f(20))) = f(f(25))
+
+//    f(25) = 5 + 25 = 30
+//    f(f(f(20))) = f(f(25)) = f(30)
+
+//    f(30) = 5 + 30 = 35
+//    f(f(f(20))) = f(f(25)) = f(30) = 35
+
+
+    public static void main(String[] args) {
+
+    }
+}
