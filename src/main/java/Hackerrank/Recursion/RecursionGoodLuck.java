ee4059ad8e57d15b9f14b984e68d6031bebba992
orloveste <orlovevm@yahoo.com>
2020-04-14 12:18:12 +0300

    HackerRank - Tutorial Recursion 3 - summation and factorial

--- src/main/java/Hackerrank/Recursion/RecursionGoodLuck.java
+++ src/main/java/Hackerrank/Recursion/RecursionGoodLuck.java
@@ -15,8 +15,37 @@ public class RecursionGoodLuck {
 //    f(f(f(20))) = f(f(25)) = f(30) = 35
 
 //    5+4+3+2+1
+    /*3 + 2 + 1*/
+    public static int Summation(int n){
+        if ( n <= 0 ) { //Base case - end
+            return 0; // additive identity property
+        }
+        else { //Recursive case - loop
+            /*3 + Summation(2);
+            3 + 2 + Summation(1);
+            3 + 2 + 1 + Summation(0);
+            3 + 2 + 1 + 0 = 6;*/
+
+            return n + Summation(n-1);
+        }
+    }
+    // 5! --> 5 * 4 * 3 * 2 * 1 --> 5 * 4!
+    // 4! --> 4 * 3 * 2 * 1
+    public static int Factorial(int n){
+        if ( n <= 1){ // Base case
+            return  1;
+        } // recursive case
+        else {
+//            Factorial(4) = 4 * Factorial(3);
+//            4 * 3 * Factorial(2);
+//            4 * 3 * 2 * Factorial(1);
+//            4 * 3 * 2 * 1;
+            return n * Factorial(n-1);
+        }
+    }
 
     public static void main(String[] args) {
+        System.out.println(Summation(3));
 
     }
 }
