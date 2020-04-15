13f0a5f6ce69355491cb293ed93791f82406a465
orloveste <orlovevm@yahoo.com>
2020-04-15 21:49:51 +0300

    HackerRank - Day10: Binary move from 10base - todo list and check longest series of 1111

---
+++ src/main/java/Hackerrank/Binary_Numbers/Day10.java
@@ -0,0 +1,16 @@
+package Hackerrank.Binary_Numbers;
+
+import java.util.Scanner;
+
+// max binary series of 1111
+public class Day10 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int numberToCheck = reader.nextInt();
+        System.out.println(Integer.toBinaryString(numberToCheck));
+        System.out.println(numberToCheck);
+        //toDO list and check the longest series of 1111
+    }
+}
+
